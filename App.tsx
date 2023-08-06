020b9284ceb6419415f7c1db772be289fbe0404e
ItIsEternity <lavrovslava207@gmail.com>
2023-08-06 11:13:33 +0300

    prettier

--- App.tsx
+++ App.tsx
@@ -1,43 +1,36 @@
-import React from "react";
-import { Text } from "react-native";
-import { FieldTime } from "./src/RNCore/components/fields/FieldTime";
-import { Col1 } from "./src/RNCore/components/markup/markup";
-import { EDate } from "./src/RNCore/sugar/date";
+import React from 'react'
+import { Text } from 'react-native'
+import { FieldTime } from './src/RNCore/components/fields/FieldTime'
+import { Col1 } from './src/RNCore/components/markup/markup'
+import { EDate } from './src/RNCore/sugar/date'
 
 export default function App() {
     return (
         <Col1 class={'m-5'}>
-            <Text>Привет! Ниже должно быть по больше примеров использования готового компонента. </Text>
+            <Text>
+                Привет! Ниже должно быть по больше примеров использования
+                готового компонента.{' '}
+            </Text>
             <Text>Разной ширины, с разным фоном и цветом шрифта</Text>
-                <FieldTime
-                    class={'mt-2 bg-primary'}
-                    value={null}
-                />
-                <FieldTime
-                    class={'mt-2 bg-danger'}
-                    value={new EDate().isoDate()}
-                />
-                <FieldTime
-                    value={null}
-                    class={'w-50 mt-2 bg-success'}
-                />
-                <FieldTime
-                    value={null}
-                    class={'w-25 mt-2 bg-black text-white'}
-                    useButtons
-                />
-                <FieldTime
-                    value={"12:12"}
-                    class="mt-2 w-100 bg-silverLight text-primary"
-                    useButtons
-                />
-                  <FieldTime
-                    value={"12:12"}
-                    class="mt-2 bg-body text-primary"
-                    useButtons
-                    onChange={console.log}
-                />
+            <FieldTime class={'mt-2 bg-primary'} value={null} />
+            <FieldTime class={'mt-2 bg-danger'} value={new EDate().isoDate()} />
+            <FieldTime value={null} class={'w-50 mt-2 bg-success'} />
+            <FieldTime
+                value={null}
+                class={'w-25 mt-2 bg-black text-white'}
+                useButtons
+            />
+            <FieldTime
+                value={'12:12'}
+                class='mt-2 w-100 bg-silverLight text-primary'
+                useButtons
+            />
+            <FieldTime
+                value={'12:12'}
+                class='mt-2 bg-body text-primary'
+                useButtons
+                onChange={console.log}
+            />
         </Col1>
-
-    );
+    )
 }
--- src/RNCore/components/fields/FieldTime.tsx
+++ src/RNCore/components/fields/FieldTime.tsx
@@ -1,9 +1,9 @@
-import { FontAwesome } from "@expo/vector-icons";
-import { useRef, useState } from "react";
-import { Pressable, TextInput, View } from "react-native";
+import { FontAwesome } from '@expo/vector-icons'
+import { useRef, useState } from 'react'
+import { Pressable, TextInput, View } from 'react-native'
 
-import tw from "../../libs/tailwind";
-import { zfill } from "../../sugar/text";
+import tw from '../../libs/tailwind'
+import { zfill } from '../../sugar/text'
 
 interface ITimeField {
     class?: string // tailwind classes
@@ -12,7 +12,7 @@ interface ITimeField {
     useButtons?: boolean // если true - отображаем кнопки "<" слева и справа ">" + или - 1 час соответственно
 }
 
-const timePattern = /^([0-2]|[01]\d:|2[0-3]:)([0-5]\d?)?$/;
+const timePattern = /^([0-2]|[01]\d:|2[0-3]:)([0-5]\d?)?$/
 
 function changeHour(time: string, shift: number) {
     if (time.length < 2) {
@@ -20,25 +20,29 @@ function changeHour(time: string, shift: number) {
     }
 
     let updated = zfill(parseInt(time.slice(0, 2)) + shift) + time.slice(2)
-    
+
     if (timePattern.test(updated)) {
         return updated
     }
-    
+
     return time
 }
 
 export function FieldTime(props: ITimeField) {
-    const [value, setValue] = useState(props?.value && timePattern.test(props.value) ? props.value : '')
-    const textInputRef = useRef<TextInput | null>(null);
+    const [value, setValue] = useState(
+        props?.value && timePattern.test(props.value) ? props.value : '',
+    )
+    const textInputRef = useRef<TextInput | null>(null)
 
     function handleChange(text: string) {
-        let formattedText = text.replace(/[^0-9]/g, "")
-        
+        let formattedText = text.replace(/[^0-9]/g, '')
+
         if (text.length === 2 && value.length > text.length) {
             formattedText = formattedText.slice(0, 1)
         } else if (text.length >= 2) {
-            formattedText = `${formattedText.slice(0, 2)}:${formattedText.slice(2)}`
+            formattedText = `${formattedText.slice(0, 2)}:${formattedText.slice(
+                2,
+            )}`
         }
 
         if (formattedText.length === 0 || timePattern.test(formattedText)) {
@@ -47,38 +51,50 @@ export function FieldTime(props: ITimeField) {
     }
 
     function handleBlur(text: string) {
-        return props?.onChange && text.length === 5 && timePattern.test(text) && props.onChange(text)
+        return (
+            props?.onChange &&
+            text.length === 5 &&
+            timePattern.test(text) &&
+            props.onChange(text)
+        )
     }
 
     function handleAddHour() {
         setValue(prev => changeHour(prev, 1))
-    };
+    }
 
     function handleSubtractHour() {
         setValue(prev => changeHour(prev, -1))
-    };
+    }
 
-    return <View style={tw`flex flex-row gap-2 items-center ${props.class || ''}`}>
-    {props.useButtons && (
-        <Pressable onPress={handleSubtractHour} onBlur={() => handleBlur(value)}>
-            <FontAwesome name="minus" style={tw`text-inherit`} />
-        </Pressable>
-      )}
-    <TextInput
-        ref={textInputRef}
-        placeholder="--:--"
-        inputMode="numeric"
-        maxLength={5}
-        value={value}
-        onChangeText={handleChange}
-        onBlur={({nativeEvent}) => handleBlur(nativeEvent.text)}
-        style={tw`w-full text-inherit`}
-    />
-      {props.useButtons && (
-        <Pressable onPress={handleAddHour} onBlur={() => handleBlur(value)}>
-            <FontAwesome name="plus" style={tw`text-inherit`} />
-        </Pressable>
-      )}
-    </View>
+    return (
+        <View style={tw`flex flex-row gap-2 items-center ${props.class || ''}`}>
+            {props.useButtons && (
+                <Pressable
+                    onPress={handleSubtractHour}
+                    onBlur={() => handleBlur(value)}
+                >
+                    <FontAwesome name='minus' style={tw`text-inherit`} />
+                </Pressable>
+            )}
+            <TextInput
+                ref={textInputRef}
+                placeholder='--:--'
+                inputMode='numeric'
+                maxLength={5}
+                value={value}
+                onChangeText={handleChange}
+                onBlur={({ nativeEvent }) => handleBlur(nativeEvent.text)}
+                style={tw`w-full text-inherit`}
+            />
+            {props.useButtons && (
+                <Pressable
+                    onPress={handleAddHour}
+                    onBlur={() => handleBlur(value)}
+                >
+                    <FontAwesome name='plus' style={tw`text-inherit`} />
+                </Pressable>
+            )}
+        </View>
+    )
 }
-
